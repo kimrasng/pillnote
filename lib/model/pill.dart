f95c86a7ebe00ea1885dd9643f7d6e9d674ccb18
kimrasng <dohyun2008@kakao.com>
2026-02-11 16:49:01 +0900

    feat: define data models for pills, user intake, and logs
    - Delete the default widget test.
    - Define the `Pill` model to store medication information, including serialization logic.
    - Introduce `UserPill` to manage user-specific medication schedules and dosages.
    - Add `PillTakeLog` to record medication intake history.

--- lib/model/pill.dart
+++ lib/model/pill.dart
@@ -2,20 +2,103 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 
+
+// 약 정보
 class Pill {
+  static int _nextid = 0;
+
   final int id;
-  final int count;  // 개수
+  final int count; // 개수
   // (공공데이터 포탈 기준)
-  final String name;  // 약품 이름
-  final int PilCode;  // 약품 코드
+  final String name; // 약품 이름
+  final int pilCode; // 약품 코드
   final String effect; // 효능
   final String guid; // 사용법
-  final String warning;  // 주의사항 경고
-  final String caution;  // 주의사항
-  final String SideEffect // 부작용
+  final String warning; // 주의사항 경고
+  final String caution; // 주의사항
+  final String sideEffect; // 부작용
   final String img;
+
+  Pill({
+    int? id,
+    required this.count,
+    required this.name,
+    required this.pilCode,
+    required this.effect,
+    required this.guid,
+    required this.warning,
+    required this.caution,
+    required this.sideEffect,
+    required this.img,
+  }) : id = id ?? _nextid++;
+
+  Pill.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        count = json['count'],
+        name = json['name'],
+        pilCode = json['pilCode'],
+        effect = json['effect'],
+        guid = json['guid'],
+        warning = json['warning'],
+        caution = json['caution'],
+        sideEffect = json['sideEffect'],
+        img = json['img'];
+
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'count': count,
+    'name': name,
+    'pilCode': pilCode,
+    'effect': effect,
+    'guid': guid,
+    'warning': warning,
+    'caution': caution,
+    'sideEffect': sideEffect,
+    'img': img
+  };
 }
 
-class UserData {
-  final 3
+// 복용 관련 정보(어떤 약을 먹는지 하루에 얼마나 먹는지 등)
+class UserPill {
+  static int _nextid = 0;
+
+  final int id;
+  final int pillId; // Pill.id 참조
+  final DateTime startDate;
+  final DateTime? endDate;
+  final int dosePerTake;  // 1회 복용양
+  final int timesPerDay;  // 1일 복용횟
+  final List<TimeOfDay> alarmTimes; // 알람 시간
+  final bool isActive;  // 활성상태 약을 복용 할지 말지
+
+  UserPill({
+    int? id,
+    required this.pillId,
+    required this.startDate,
+    this.endDate,
+    required this.dosePerTake,
+    required this.timesPerDay,
+    required this.alarmTimes,
+    this.isActive = true
+  }) : id = id ?? _nextid++;
+}
+
+
+// 복용 기록
+class PillTakeLog {
+  static int _nextid = 0;
+
+  final int id;
+  final int userPillId; // UserPill.id 참조
+  final DateTime scheduledTime; // 예정 섭취시간 alaramTimes(AlaramSettings) 에 있는거 기준
+  final DateTime? takeTime; // 복용 시간 사용자가 복용 클릭한 기준 시간
+  final bool isTake;
+
+  PillTakeLog({
+    int? id,
+    required this.userPillId,
+    required this.scheduledTime,
+    required this.takeTime,
+    this.isTake = false,
+  }) : id = id ?? _nextid++;
 }
